3f482f3379913210b03168a8264ab8cbca4f4f9d
Israel Waldner <60708054+mordy-python@users.noreply.github.com>
2022-12-19 00:50:24 +0000

    Added icon and avatar and removed deno avatatr

--- index.ts
+++ index.ts
@@ -4,12 +4,13 @@ import blog from "https://deno.land/x/blog/blog.tsx";
 blog({
   title: "Israel's Blog",
   author: "Israel",
-  avatar: "./avatar.svg",
-  avatarClass: "rounded-full",
+  avatar: "./avatar.png",
+  avatarClass: "full",
   dateStyle: "long",
   links: [
     { title: "Email", url: "mailto:imky171@gmail.com" },
     { title: "GitHub", url: "https://github.com/mordy-python" },
   ],
+    favicon: "avatar.png",
   background: "#282a36"
 });
